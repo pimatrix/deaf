d4f40da994b2e4d0a92236212f65dd38d9b966ef
djx <dongjinxianaugust@gmail.com>
2016-05-29 11:03:11 +0800

    temp log module, will modif later

--- joke.go
+++ joke.go
@@ -1,7 +1,77 @@
+// Copyright 2016 The go-deaf Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the go LICENSE file.
+
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Printf("hello world, joke! \n")
+// }
+
 package main
 
-import "fmt"
+import (
+    "github.com/dongjinxian/deaf/log"
+)
 
 func main() {
-    fmt.Printf("hello world, joke! \n")
+
+    name := "djx"
+    log.Debug("My name is %v", name)
+    log.Release("My name is %v", name)
+    log.Error("My name is %v", name)
+    // log.Fatal("My name is %v", name)
+
+    // logger, err := log.New("debug", "test_djx_log")
+    // if err != nil {
+    //     return
+    // }
+    // defer logger.Close()
+
+    log.Debug("will not print")
+    log.Release("My name is %v", name)
+
+    // log.Export(logger)
+
+    log.Debug("will not print")
+    log.Release("My name is %v", name)
 }
+
+
+// package main
+
+// import (
+//     "fmt"
+//     "log"
+//     "os"
+// )
+
+// func main() {
+
+//     fmt.Println("begin TestLog ...")
+//     file, err := os.Create("test_djx.log")
+//     if err != nil {
+//         log.Fatalln("fail to create test.log file!")
+//     }
+//     logger := log.New(file, "", log.LstdFlags|log.Llongfile)
+//     log.Println("1.Println log with log.LstdFlags ...")
+//     logger.Println("1.Println log with log.LstdFlags ...")
+
+//     logger.SetFlags(log.LstdFlags)
+
+//     log.Println("2.Println log without log.LstdFlags ...")
+//     logger.Println("2.Println log without log.LstdFlags ...")
+
+//     //log.Panicln("3.std Panicln log without log.LstdFlags ...")
+//     //fmt.Println("3 Will this statement be execute ?")
+//     //logger.Panicln("3.Panicln log without log.LstdFlags ...")
+
+//     log.Println("4.Println log without log.LstdFlags ...")
+//     logger.Println("4.Println log without log.LstdFlags ...")
+
+//     log.Fatal("5.std Fatal log without log.LstdFlags ...")
+//     fmt.Println("5 Will this statement be execute ?")
+//     logger.Fatal("5.Fatal log without log.LstdFlags ...")
+// }
---
+++ log/log.go
@@ -0,0 +1,176 @@
+// Copyright 2016 The go-deaf Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the go LICENSE file.
+
+// 目前仅仅是一个临时的实现，以后会大幅删改
+// // 日志管道
+// log_ch := make([]chan string, 1024)
+
+package log
+
+import (
+    // "errors"
+    "fmt"
+    "log"
+    "os"
+    "path"
+    "strings"
+    "time"
+)
+
+// levels
+const (
+    kDebug = 0
+    kInfo  = 1
+    kWarn  = 2
+    kError = 3
+)
+
+const (
+    kDebugName = "[Deg] "
+    kInfoName  = "[Inf] "
+    kWarnName  = "[War] "
+    kErrorName = "[Err] "
+)
+
+type Logger struct {
+    level      int
+    baseLogger *log.Logger
+    baseFile   *os.File
+}
+
+// 获取日志等级：不正确的日志等级，设默认信息级别
+func get_log_level(levelname string) (level int) {
+
+    switch strings.ToLower(levelname) {
+    case "debug":
+        level = kDebug
+    case "info":
+        level = kInfo
+    case "warn":
+        level = kWarn
+    case "error":
+        level = kError
+    default:
+        level = kInfo
+    }
+
+    // 返回
+    return level
+}
+
+func New(levelname string, pathname string) (*Logger, error) {
+
+    // 日志等级
+    level := get_log_level(levelname)
+
+    // logger
+    var baseLogger *log.Logger
+    var baseFile *os.File
+
+    now := time.Now()
+    filename := fmt.Sprintf("%d%02d%02d_%02d_%02d_%02d.log",
+     now.Year(), now.Month(), now.Day(), now.Hour(),
+      now.Minute(), now.Second())
+
+    file, err := os.Create(path.Join(pathname, filename))
+    if err != nil {
+        panic(err)
+    }
+
+    baseFile = file
+    baseLogger = log.New(file, "", log.LstdFlags|log.Llongfile)
+
+    // 校验
+    if baseLogger == nil {
+        panic("baseLogger is nil")
+    }
+    if baseFile == nil {
+        panic("baseFile is nil")
+    }
+
+    // new
+    logger := new(Logger)
+    logger.level = level
+    logger.baseLogger = baseLogger
+    logger.baseFile = baseFile
+
+    return logger, nil
+}
+
+// It's dangerous to call the method on logging
+func (logger *Logger) Close() {
+
+    if logger.baseFile != nil {
+        logger.baseFile.Close()
+    }
+
+    logger.baseLogger = nil
+    logger.baseFile = nil
+}
+
+func (logger *Logger) doPrintf(level int, levelname string, format string, a ...interface{}) {
+
+    if level < logger.level {
+        return
+    }
+    if logger.baseLogger == nil {
+        panic("logger closed")
+    }
+
+    format = levelname + format
+    logger.baseLogger.Printf(format, a...)
+
+    if level > kInfo {
+       log.Printf(format, a...) 
+    }
+
+    if level == kError {
+        os.Exit(1)
+    }
+}
+
+func (logger *Logger) Debug(format string, a ...interface{}) {
+    logger.doPrintf(kDebug, kDebugName, format, a...)
+}
+
+func (logger *Logger) Release(format string, a ...interface{}) {
+    logger.doPrintf(kInfo, kInfoName, format, a...)
+}
+
+func (logger *Logger) Error(format string, a ...interface{}) {
+    logger.doPrintf(kWarn, kWarnName, format, a...)
+}
+
+func (logger *Logger) Fatal(format string, a ...interface{}) {
+    logger.doPrintf(kError, kErrorName, format, a...)
+}
+
+var gLogger, _ = New("debug", "")
+
+// It's dangerous to call the method on logging
+func Export(logger *Logger) {
+    if logger != nil {
+        gLogger = logger
+    }
+}
+
+func Debug(format string, a ...interface{}) {
+    gLogger.Debug(format, a...)
+}
+
+func Release(format string, a ...interface{}) {
+    gLogger.Release(format, a...)
+}
+
+func Error(format string, a ...interface{}) {
+    gLogger.Error(format, a...)
+}
+
+func Fatal(format string, a ...interface{}) {
+    gLogger.Fatal(format, a...)
+}
+
+func Close() {
+    gLogger.Close()
+}
